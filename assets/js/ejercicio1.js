1cbeff1be7d34caf771a040e44979adb80c76496
Diego Cabre <diegocabre@gmail.com>
2024-03-21 10:58:44 -0300

    borrado de errores

--- assets/js/ejercicio1.js
+++ assets/js/ejercicio1.js
@@ -19,30 +19,29 @@ function enviarFormulario(e) {
   let letrasRegex = /^[a-zA-Z\s]*$/;
 
   // Validación de campos vacíos y solo letras
-  if (nombre.value !== "" && asunto.value !== "" && mensaje.value !== "") {
-    if (letrasRegex.test(nombre.value) && letrasRegex.test(asunto.value)) {
-      // Si todos los campos tienen contenido y solo contienen letras
-      resultado.innerHTML = "Mensaje enviado con éxito !!!";
+  if (
+    nombre.value !== "" &&
+    asunto.value !== "" &&
+    mensaje.value !== "" &&
+    letrasRegex.test(nombre.value) &&
+    letrasRegex.test(asunto.value)
+  ) {
+    // Si todos los campos tienen contenido y solo contienen letras
+    resultado.innerHTML = "Mensaje enviado con éxito !!!";
 
-      // Limpia el formulario
-      formulario.reset();
-    } else {
-      // Si los campos no contienen solo letras
+    // Limpia los campos
+    nombre.value = "";
+    asunto.value = "";
+    mensaje.value = "";
 
-      errorNombre.innerHTML = "el nombre debe ser solo letras";
-      errorAsunto.innerHTML = "el asunto debe ser solo letras";
-      errorMensaje.innerHTML = "el mensaje debe ser solo letras";
-
-      // Limpia el formulario
-      formulario.reset();
-    }
+    // Limpia los errores
+    errorNombre.innerHTML = "";
+    errorAsunto.innerHTML = "";
+    errorMensaje.innerHTML = "";
   } else {
     // Si los campos estan vacíos
-    errorNombre.innerHTML = "el nombre es requerido";
-    errorAsunto.innerHTML = "el asunto es requerido";
+    errorNombre.innerHTML = "el nombre es requerido y con solo letras";
+    errorAsunto.innerHTML = "el asunto es requerido y con solo letras";
     errorMensaje.innerHTML = "el mensaje es requerido";
-
-    // Limpia el formulario
-    formulario.reset();
   }
 }
